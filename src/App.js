ded26e092e3c8810fde8a32a02a5981c8982860f
Jacek <jacuskozlowski@gmail.com>
2024-09-20 11:12:17 +0200

    feat: added navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link, NavLink } from "react-router-dom";
 import Home from "./pages/home";
 import CreatePost from "./pages/createpost";
 
@@ -7,8 +7,10 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Link to="/createpost"> Create a Post </Link>
-        <Link to="/"> Home Page </Link>
+        <nav>
+          <NavLink to="/" activeClassName="active"> Home Page </NavLink>
+          <NavLink to="/createpost" activeClassName="active"> Create a Post </NavLink>
+        </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/createpost" element={<CreatePost />} />
